29bdd65df18e04af736b8bb8125125cd2bd61cd7
Brandon Horst <brandonhorst@gmail.com>
2016-10-19 11:16:33 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var _ = require('lodash')
+
+var key = {
+  validate: function (obj, schema) {
+    return (
+      _.isPlainObject(obj) &&
+      _.isNumber(obj.key) && obj.key >= 0 &&
+      _.isString(obj.string) &&
+      _.isString(obj.display) &&
+      _.chain(obj).keys().difference(['key', 'string', 'ctrl', 'alt', 'shift', 'cmd', 'display']).isEmpty().value() &&
+      (_.isUndefined(obj.ctrl) || _.isBoolean(obj.ctrl)) &&
+      (_.isUndefined(obj.alt) || _.isBoolean(obj.alt)) &&
+      (_.isUndefined(obj.shift) || _.isBoolean(obj.shift)) &&
+      (_.isUndefined(obj.cmd) || _.isBoolean(obj.cmd))
+    )
+  },
+  validateSchema: function (schema) {
+    return true
+  }
+}
+
+var auth = {
+  validate (obj, schema) {
+    return _.isPlainObject(obj)
+  },
+  validateSchema (schema) {
+    return _.isPlainObject(schema) && _.isString(schema.method)
+  }
+}
+
+module.exports = {
+  key: key,
+  auth: auth
+}
